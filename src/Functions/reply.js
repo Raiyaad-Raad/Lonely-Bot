e256cfa81e39aa8e3f7b1b21f9b8b4ed162a71db
Raiyaad Raad <77395366+Raiyaad-Raad@users.noreply.github.com>
2024-11-13 13:22:08 +0600

    আয়

--- src/Functions/reply.js
+++ src/Functions/reply.js
@@ -1,11 +1,14 @@
 const { EmbedBuilder } = require('discord.js');
 
+const { client } = require('../cool');
+
 function reply(interaction, color, description) {
   interaction.reply({
     embeds: [
       new EmbedBuilder()
+      .setAuthor({ name: `${client.user.name}`, iconURL: `${client.user.avatarURL}` })
       .setColor(`${color}`)
       .setDescription(`${description}`)
     ]
-  })
+  });
 }
--- src/cool.js
+++ src/cool.js
@@ -1,22 +1,22 @@
-const { Client, Partials, Collection } = require("discord.js")
-const dotenv = require("dotenv").config()
-const { promisify } = require('util')
-const { glob } = require('glob')
-const PG = promisify(glob)
-const Ascii = require("ascii-table")
-const ms = require('ms')
-const { Channel, GuildMember, Message, Reaction, ThreadMember, User, GuildScheduledEvent } = Partials
+const { Client, Partials, Collection } = require("discord.js");
+const dotenv = require("dotenv").config();
+const { promisify } = require('util');
+const { glob } = require('glob');
+const PG = promisify(glob);
+const Ascii = require("ascii-table");
+const ms = require('ms');
+const { Channel, GuildMember, Message, Reaction, ThreadMember, User, GuildScheduledEvent } = Partials;
 const client = new Client({
   intents: 3276799
-})
+});
 
-module.exports = client
+module.exports = client;
 
-require("./Handlers/Commands.js")(client, PG, Ascii, ms)
-require("./Handlers/Events.js")(client, PG, Ascii, ms)
+require("./Handlers/Commands.js")(client, PG, Ascii, ms);
+require("./Handlers/Events.js")(client, PG, Ascii, ms);
 
 client.commands = new Collection();
 client.events = new Collection();
 
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
