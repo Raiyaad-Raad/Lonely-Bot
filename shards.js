96825f915eef322a949ee32b636b9be5c8836117
raad <raiyaad.raad@proton.me>
2024-11-22 16:24:07 +0600

    Fixed Shards Not Onlining

--- shards.js
+++ shards.js
@@ -13,3 +13,5 @@ const shardManager = new ShardingManager(
 shardManager.on('shardCreate', (shard) => {
     console.log(chalk.blue, chalk.bgBlue('[SHARDS]: '), chalk.bold(`Launched shard: ${shard.id}`))
 })
+
+shardManager.spawn();
