21a45d9ca847094fe083976f29f8b84fb7794220
Raiyaad Raad <77395366+Raiyaad-Raad@users.noreply.github.com>
2024-11-22 18:45:33 +0600

    Update slap.js

--- Commands/Information/slap.js
+++ Commands/Information/slap.js
@@ -10,44 +10,6 @@ module.exports = {
     **/
     async execute(interaction, client) {
       // code here
-const { Client, Intents, SlashCommandBuilder } = require('discord.js');
-const { token } = require('./config.json'); // Assume your bot token is in a config file
-
-// Create a new bot client with required intents
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.once('ready', () => {
-  console.log('Bot is online!');
-});
-
-// Register commands when the bot starts up
-client.on('ready', () => {
-  const commands = client.guilds.cache.get('YOUR_GUILD_ID')?.commands;
-
-  if (commands) {
-    commands.create(
-      new SlashCommandBuilder()
-        .setName('slap')
-        .setDescription('Sends a slap message!')
-        .toJSON()
-    ).then(() => console.log('Slash command registered!'))
-      .catch(console.error);
-  }
-});
-
-// Handle interaction events (e.g., when the user uses the slash command)
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  if (commandName === 'slap') {
-    await interaction.reply('Here’s a slap! 👋');
-  }
-});
-
-// Login with your bot token
-client.login(token);
-
+        await interaction.reply({ text: "hers a slap" });
     }
 }
