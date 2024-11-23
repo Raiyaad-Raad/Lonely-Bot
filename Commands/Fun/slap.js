392ff4c526c5fc514c8fcbe0f3cf116c96d0fd6c
z1HypersXz <serverpromc200173@gmail.com>
2024-11-23 20:18:35 +0600

    Update slap.js

--- Commands/Fun/slap.js
+++ Commands/Fun/slap.js
@@ -1,9 +1,9 @@
-const { Client, ChatInputCommandInteraction, EmbedBuilder } = require('discord.js');
+const { Client, ChatInputCommandInteraction } = require('discord.js');
 
 module.exports = {
-    name: "slap", // command name here
-    description: "Slap someone and let the bot announce it!", // command description here
-    category: "Fun", // command category here
+    name: "slap", // command name
+    description: "Slap someone with a funny image!", // command description
+    category: "Fun", // command category
     options: [
         {
             name: "user", // option name
@@ -17,12 +17,21 @@ module.exports = {
     * @param {ChatInputCommandInteraction} interaction
     **/
     async execute(interaction, client) {
+        // Predefined slap images
+        const slapImages = [
+            "https://i.imgur.com/fm49srQ.jpg", // Replace these with your desired slap images
+            "https://i.imgur.com/WxNkKpF.jpg",
+            "https://i.imgur.com/4oLIrwj.jpg",
+            "https://i.imgur.com/ovZUl6P.jpg",
+            "https://i.imgur.com/Nm6xDYM.jpg",
+        ];
+
         // Get the user who invoked the command
         const slapper = interaction.user;
         // Get the target user from the options
         const targetUser = interaction.options.getUser("user");
 
-        // Check if the slapper is trying to slap themselves
+        // Prevent self-slapping
         if (slapper.id === targetUser.id) {
             return interaction.reply({
                 content: "You can't slap yourself! 🤦",
@@ -30,9 +39,18 @@ module.exports = {
             });
         }
 
+        // Pick a random slap image
+        const randomImage = slapImages[Math.floor(Math.random() * slapImages.length)];
+
         // Construct the response
         await interaction.reply({
             content: `${slapper} gave a big slap to ${targetUser}! 👋`,
+            embeds: [
+                {
+                    color: 0xff0000, // Set an embed color (red for slap)
+                    image: { url: randomImage }, // Attach the random image
+                },
+            ],
             allowedMentions: { users: [slapper.id, targetUser.id] },
         });
     },
