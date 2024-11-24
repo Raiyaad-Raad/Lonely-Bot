42c7ea58776583168469ddd8c273e07119523e47
raad <raiyaad.raad@proton.me>
2024-11-24 21:18:31 +0600

    Tried fixing slap

--- Commands/Fun/slap.js
+++ Commands/Fun/slap.js
@@ -1,53 +1,117 @@
-const { Client, ChatInputCommandInteraction, EmbedBuilder } = require('discord.js')
+// const { Client, ChatInputCommandInteraction, EmbedBuilder } = require('discord.js')
+// const axios = require('axios');
+
+// module.exports = {
+//     name: "slap", // command name here
+//     description: "Slap someone!", // command description here
+//     category: "🎈Fun", // command category here
+    // options: [
+    //     {
+    //         name: "user",
+    //         description: "mention the user you want to slap",
+    //         required: true,
+    //         type: 6,
+    //       },
+    // ],
+//     /**
+//     * @param {Client} client
+//     * @param {ChatInputCommandInteraction} interaction
+//     **/
+//     async execute(interaction, client) {
+//       const user = interaction.options.getUser('user');
+
+//       const randomPos = Math.floor(Math.random() * 5000);
+
+//       const apiResponse = await axios.get('https://g.tenor.com/v1/search', {
+//         params: {
+//             q: 'anime slap',
+//             key: 'AIzaSyDSJzSFsg8qoxmS00_Ay1RsZps6iT5UmiQ',
+//             limit: 1,
+//             pos: randomPos
+//         }
+//     });
+
+//       const gifs = apiResponse.data.results;
+//       if (gifs.length === 0) {
+//         return interaction.reply({ content: "Error Occured!, Try Again", ephemeral: true }).then(() => {
+//           console.log("Error Searching Gifs. - Tenor API Erorr")
+//       });
+//       };
+
+//       const slapURL = gifs[0].media[0].gif.url;
+
+//       const embed = new EmbedBuilder()
+//                 .setColor("Random")
+//                 .setDescription(`${interaction.user} slapped ${user}!`)
+//                 .setImage(slapURL)
+//                 .setTimestamp();
+
+//       interaction.reply({
+//         embeds: [embed]
+//       })
+//   }
+// }
+const { Client, ChatInputCommandInteraction, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 
 module.exports = {
-    name: "slap", // command name here
-    description: "Slap someone!", // command description here
-    category: "🎈Fun", // command category here
+    name: "slap", // command name
+    description: "Slap a user with a random anime gif!", // command description
+    category: "Fun", // command category
     options: [
-        {
-            name: "user",
-            description: "mention the user you want to slap",
-            required: true,
-            type: 6,
-          },
-    ],
+      {
+          name: "user",
+          description: "mention the user you want to slap",
+          required: true,
+          type: 6,
+        },
+  ],
     /**
-    * @param {Client} client
-    * @param {ChatInputCommandInteraction} interaction
-    **/
+     * @param {Client} client
+     * @param {ChatInputCommandInteraction} interaction
+     **/
     async execute(interaction, client) {
-      const user = interaction.options.getUser('user');
+        // Fetch the target user
+        const targetUser = interaction.options.getUser('user');
 
-      const randomPos = Math.floor(Math.random() * 5000);
-
-      const apiResponse = await axios.get('https://g.tenor.com/v1/search', {
-        params: {
-            q: 'anime slap',
-            key: 'AIzaSyDSJzSFsg8qoxmS00_Ay1RsZps6iT5UmiQ',
-            limit: 1,
-            pos: randomPos
+        if (!targetUser) {
+            return interaction.reply({ content: "You need to mention someone to slap!", ephemeral: true });
         }
-    });
 
-      const gifs = apiResponse.data.results;
-      if (gifs.length === 0) {
-        return interaction.reply({ content: "Error Occured!, Try Again", ephemeral: true }).then(() => {
-          console.log("Error Searching Gifs. - Tenor API Erorr")
-      });
-      };
+        try {
+            // Generate a random offset for infinite randomness
+            const randomPos = Math.floor(Math.random() * 5000); // Random position between 0 and 5000
 
-      const slapURL = gifs[0].media[0].gif.url;
+            // Fetch GIFs from Tenor API with random offset
+            const response = await axios.get('https://g.tenor.com/v1/search', {
+                params: {
+                    q: 'anime slap',
+                    key: 'YOUR_TENOR_API_KEY', // Replace with your Tenor API Key
+                    limit: 1, // Fetch only one GIF at the random position
+                    pos: randomPos // Random position in search results
+                }
+            });
 
-      const embed = new EmbedBuilder()
+            const gifs = response.data.results;
+
+            if (gifs.length === 0) {
+                return interaction.reply({ content: "Couldn't find a slap GIF, try again!", ephemeral: true });
+            }
+
+            const gifUrl = gifs[0].media[0].gif.url;
+
+            // Create the embed
+            const embed = new EmbedBuilder()
                 .setColor("Random")
-                .setDescription(`${interaction.user} slapped ${user}!`)
-                .setImage(slapURL)
+                .setDescription(`${interaction.user} slapped ${targetUser}!`)
+                .setImage(gifUrl)
                 .setTimestamp();
 
-      interaction.reply({
-        embeds: [embed]
-      })
-  }
-}
+            // Send the embed
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error(error);
+            interaction.reply({ content: "Something went wrong while fetching the slap GIF!", ephemeral: true });
+        }
+    }
+};
